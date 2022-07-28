1bc3c6f33175020fa044aa8bce456b26556f6e4f
Raghu K <raghuk.vit@gmail.com>
2022-07-28 06:57:38 +0530

    Update terraform/tf-module-vpc/

--- routes.tf
+++ routes.tf
@@ -26,3 +26,8 @@ resource "aws_route_table_association" "public" {
   route_table_id = aws_route_table.public.id
 }
 
+resource "aws_route" "igw-route-to-public-subnets" {
+  route_table_id         = aws_route_table.public.id
+  destination_cidr_block = "0.0.0.0/0"
+  gateway_id             = aws_internet_gateway.igw.id
+}
