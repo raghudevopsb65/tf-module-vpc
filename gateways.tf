16b7b266d1c13e1b379c6d7676d0aaa203c794bd
Raghu K <raghuk.vit@gmail.com>
2022-07-28 07:02:26 +0530

    Update terraform/tf-module-vpc/

--- gateways.tf
+++ gateways.tf
@@ -6,3 +6,16 @@ resource "aws_internet_gateway" "igw" {
   }
 }
 
+
+resource "aws_eip" "ngw" {}
+
+
+resource "aws_nat_gateway" "ngw" {
+  allocation_id = aws_eip.ngw.id
+  subnet_id     = aws_subnet.public.*.id[0]
+
+  tags = {
+    Name = "${var.PROJECT}-${var.ENV}-ngw"
+  }
+}
+
--- routes.tf
+++ routes.tf
@@ -31,3 +31,9 @@ resource "aws_route" "igw-route-to-public-subnets" {
   destination_cidr_block = "0.0.0.0/0"
   gateway_id             = aws_internet_gateway.igw.id
 }
+
+resource "aws_route" "ngw-route-to-private-subnets" {
+  route_table_id         = aws_route_table.private.id
+  destination_cidr_block = "0.0.0.0/0"
+  nat_gateway_id         = aws_nat_gateway.ngw.id
+}
