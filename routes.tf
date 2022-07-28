eee766594f07a73ec632d0efa1b9763dd7f44f15
Raghu K <raghuk.vit@gmail.com>
2022-07-28 07:17:05 +0530

    Update terraform/tf-module-vpc/

--- routes.tf
+++ routes.tf
@@ -37,3 +37,22 @@ resource "aws_route" "ngw-route-to-private-subnets" {
   destination_cidr_block = "0.0.0.0/0"
   nat_gateway_id         = aws_nat_gateway.ngw.id
 }
+
+resource "aws_route" "peer-route-to-private-subnets" {
+  route_table_id            = aws_route_table.private.id
+  destination_cidr_block    = var.DEFAULT_VPC_CIDR
+  vpc_peering_connection_id = aws_vpc_peering_connection.peer.id
+}
+
+resource "aws_route" "peer-route-to-public-subnets" {
+  route_table_id            = aws_route_table.private.id
+  destination_cidr_block    = var.DEFAULT_VPC_CIDR
+  vpc_peering_connection_id = aws_vpc_peering_connection.peer.id
+}
+
+resource "aws_route" "peer-route-to-default-vpc-subnets" {
+  route_table_id            = var.DEFAULT_VPC_RT
+  destination_cidr_block    = var.VPC_CIDR
+  vpc_peering_connection_id = aws_vpc_peering_connection.peer.id
+}
+
