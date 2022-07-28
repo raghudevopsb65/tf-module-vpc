d7f66985802f5638f21e3480566e966b32b7dbc5
Raghu K <raghuk.vit@gmail.com>
2022-07-28 07:11:52 +0530

    Update terraform/tf-module-vpc/

--- vars.tf
+++ vars.tf
@@ -4,3 +4,6 @@ variable "ENV" {}
 variable "PRIVATE_SUBNETS_CIDR" {}
 variable "PUBLIC_SUBNETS_CIDR" {}
 variable "AZ" {}
+variable "DEFAULT_VPC_ID" {}
+variable "DEFAULT_VPC_CIDR" {}
+variable "DEFAULT_VPC_RT" {}
