93a560c9b9a40b0cbc98dbcb9e25b0a36bf83422
Raghu K <raghuk.vit@gmail.com>
2022-07-28 07:17:59 +0530

    Update terraform/tf-module-vpc/

--- routes.tf
+++ routes.tf
@@ -45,7 +45,7 @@ resource "aws_route" "peer-route-to-private-subnets" {
 }
 
 resource "aws_route" "peer-route-to-public-subnets" {
-  route_table_id            = aws_route_table.private.id
+  route_table_id            = aws_route_table.public.id
   destination_cidr_block    = var.DEFAULT_VPC_CIDR
   vpc_peering_connection_id = aws_vpc_peering_connection.peer.id
 }
